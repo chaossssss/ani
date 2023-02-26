d1b24981d1e0a6722a07e1dd23835a6956565a58
shensichao <1033729276@qq.com>
2023-02-26 15:49:18 +0800

    fix:css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,11 @@
   <!-- <canvas id="canvas" width="500" height="500" ref="svgaRef" class="ani-box"></canvas> -->
   <div id="svgaRef" ref="svgaRef" class="ani-box"></div>
   <canvas class="ani-box" id="pag"></canvas>
+  <div class="video-box">
+    <video class="video" src="../assets/fire1.mp4" autoplay muted loop></video>
+  </div>
+  <img src="../assets/chassis3.png" />
+  <img class="diff-pic" src="../assets/chassis3.png" />
 </template>
 <script setup>
 import { onMounted, ref } from 'vue'
@@ -37,14 +42,14 @@ onMounted(() => {
 
 
   PAGInit().then((PAG) => {
-  const url = './red.pag';
+  const url = '/red.pag';
   fetch(url)
     .then((response) => response.blob())
     .then(async (blob) => {
       const file = new window.File([blob], url.replace(/(.*\/)*([^.]+)/i, '$2'));
       const pagFile = await PAG.PAGFile.load(file);
-      document.getElementById('pag').width = pagFile.width();
-      document.getElementById('pag').height = pagFile.height();
+      // document.getElementById('pag').width = pagFile.width();
+      // document.getElementById('pag').height = pagFile.height();
       const pagView = await PAG.PAGView.init(pagFile, '#pag');
       pagView.setRepeatCount(0);
       await pagView.play();
@@ -58,5 +63,16 @@ onMounted(() => {
 .ani-box {
   width: 500px;
   height: 500px;
+}
+.video-box {
+  width: 800px;
+  height: 800px;
+  background: #0a12ff;
+}
+.video {
+  /* mix-blend-mode: screen; */
+}
+.diff-pic {
+  filter: hue-rotate(329deg) saturate(100%) brightness(100%);
 }
 </style>
